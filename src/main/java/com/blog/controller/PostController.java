c2e1cc7bcb1cae48fb9965d21d2574ded8c9abe0
GeonHui2 <92250144+GeonHui2@users.noreply.github.com>
2022-11-27 23:36:10 +0900

    study

--- src/main/java/com/blog/controller/PostController.java
+++ src/main/java/com/blog/controller/PostController.java
@@ -1,6 +1,10 @@
 package com.blog.controller;
 
+import com.blog.domain.Post;
 import com.blog.request.PostCreate;
+import com.blog.response.PostResponse;
+import com.blog.service.PostService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -13,22 +17,20 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PostController {
 
+    private final PostService postService;
+
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) throws Exception {
-        if (result.hasErrors()) {
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            FieldError firstFieldError = fieldErrors.get(0);
-            String fieldName = firstFieldError.getField(); // title
-            String errorMessage = firstFieldError.getDefaultMessage(); // ..에러 메시지
+    public void post(@RequestBody @Valid PostCreate request) throws Exception {
+        postService.wirte(request);
+    }
 
-            Map<String, String> error = new HashMap<>();
-            error.put(fieldName, errorMessage);
-            return error;
-        }
-
-        return Map.of();
+    @GetMapping("/posts/{postId}")
+    public PostResponse get(@PathVariable(name = "postId") Long id) {
+        PostResponse response = postService.get(id);
+        return response;
     }
 
 
--- src/main/java/com/blog/request/PostCreate.java
+++ src/main/java/com/blog/request/PostCreate.java
@@ -1,5 +1,6 @@
 package com.blog.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,4 +17,16 @@ public class PostCreate {
 
     @NotBlank(message = "컨텐츠를 입력해주세요.")
     private String content;
+
+    @Builder
+    public PostCreate(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
+
+// 빌더의 장점
+// - 가독성에 좋다.
+// - 값 생성에 대한 유연함
+// - 필요한 값만 받을 수 있다.
+// - 객체의 불변성
